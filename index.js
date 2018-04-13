89db544e672723528361f8836cf309c9f18c7155
Sally Zhao <sallyzhao@Sallys-MBP.wv.cc.cmu.edu>
2018-04-13 16:29:03 -0400

    delete selected parameters working

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(document).ready( function() {
+
 	$('#add-variable-btn').click( function() {
 		$('#variable-label').text('Selected variable');
 		$('#select-variable').hide();
@@ -7,10 +8,17 @@ $(document).ready( function() {
 		$('#add-variable-btn').text("Edit variable")
 	});
 
-
+	var deleteIndex = 0;
 	$('#add-parameter-btn').click( function() {
 		$('#parameter-label').text('Selected parameters');
 		var outcome = $('#select-parameter').val();
-		$('#parameters-selected').append('<a href="javascript:void(0);" id="delete-parameter"> X </a>' + '<p class="parameter">' + outcome + '</p>');
+		$('#parameters-selected').append('<div> <a href="javascript:void(0);" id="delete-parameter"> X </a>' + '<p class="parameter">' + outcome + '</p> </div>');
+	
 	});
+
+	$('#parameters-selected').on( 'click', '#delete-parameter', function() {
+		console.log('ih');
+		$(this).parent().remove();
+	});
+
 });
